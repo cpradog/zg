ba27537e11b1de4ac996fe21c9eabdb8d500ef37
Jonathan Raphaelson <jon@accidental.cc>
2024-08-03 22:47:13 -0600

    introduce unmanaged props data
    in situations where an allocator is already available, we can save the
    inclusion of an allocator in props data, and instead use unmanaged
    directly.
    
    ```zig
    const data = PropsData.Unmanaged.init(alloc);
    defer data.deinit(alloc);
    ```

--- src/PropsData.zig
+++ src/PropsData.zig
@@ -4,140 +4,215 @@ const compress = std.compress;
 const mem = std.mem;
 const testing = std.testing;
 
-allocator: mem.Allocator,
-core_s1: []u16 = undefined,
-core_s2: []u8 = undefined,
-props_s1: []u16 = undefined,
-props_s2: []u8 = undefined,
-num_s1: []u16 = undefined,
-num_s2: []u8 = undefined,
+pub const Unmanaged = struct {
+    core_s1: []u16 = undefined,
+    core_s2: []u8 = undefined,
+    props_s1: []u16 = undefined,
+    props_s2: []u8 = undefined,
+    num_s1: []u16 = undefined,
+    num_s2: []u8 = undefined,
+
+    pub fn init(allocator: mem.Allocator) !Unmanaged {
+        const decompressor = compress.flate.inflate.decompressor;
+        const endian = builtin.cpu.arch.endian();
+
+        // Process DerivedCoreProperties.txt
+        const core_bytes = @embedFile("core_props");
+        var core_fbs = std.io.fixedBufferStream(core_bytes);
+        var core_decomp = decompressor(.raw, core_fbs.reader());
+        var core_reader = core_decomp.reader();
+
+        var self = .{};
+
+        const core_stage_1_len: u16 = try core_reader.readInt(u16, endian);
+        self.core_s1 = try allocator.alloc(u16, core_stage_1_len);
+        errdefer allocator.free(self.core_s1);
+        for (0..core_stage_1_len) |i| self.core_s1[i] = try core_reader.readInt(u16, endian);
+
+        const core_stage_2_len: u16 = try core_reader.readInt(u16, endian);
+        self.core_s2 = try allocator.alloc(u8, core_stage_2_len);
+        errdefer allocator.free(self.core_s2);
+        _ = try core_reader.readAll(self.core_s2);
+
+        // Process PropList.txt
+        const props_bytes = @embedFile("props");
+        var props_fbs = std.io.fixedBufferStream(props_bytes);
+        var props_decomp = decompressor(.raw, props_fbs.reader());
+        var props_reader = props_decomp.reader();
+
+        const stage_1_len: u16 = try props_reader.readInt(u16, endian);
+        self.props_s1 = try allocator.alloc(u16, stage_1_len);
+        errdefer allocator.free(self.props_s1);
+        for (0..stage_1_len) |i| self.props_s1[i] = try props_reader.readInt(u16, endian);
+
+        const stage_2_len: u16 = try props_reader.readInt(u16, endian);
+        self.props_s2 = try allocator.alloc(u8, stage_2_len);
+        errdefer allocator.free(self.props_s2);
+        _ = try props_reader.readAll(self.props_s2);
+
+        // Process DerivedNumericType.txt
+        const num_bytes = @embedFile("numeric");
+        var num_fbs = std.io.fixedBufferStream(num_bytes);
+        var num_decomp = decompressor(.raw, num_fbs.reader());
+        var num_reader = num_decomp.reader();
+
+        const num_stage_1_len: u16 = try num_reader.readInt(u16, endian);
+        self.num_s1 = try allocator.alloc(u16, num_stage_1_len);
+        errdefer allocator.free(self.num_s1);
+        for (0..num_stage_1_len) |i| self.num_s1[i] = try num_reader.readInt(u16, endian);
+
+        const num_stage_2_len: u16 = try num_reader.readInt(u16, endian);
+        self.num_s2 = try allocator.alloc(u8, num_stage_2_len);
+        errdefer allocator.free(self.num_s2);
+        _ = try num_reader.readAll(self.num_s2);
+
+        return self;
+    }
+
+    pub fn deinit(self: *const Unmanaged, allocator: mem.Allocator) void {
+        allocator.free(self.core_s1);
+        allocator.free(self.core_s2);
+        allocator.free(self.props_s1);
+        allocator.free(self.props_s2);
+        allocator.free(self.num_s1);
+        allocator.free(self.num_s2);
+    }
+
+    /// True if `cp` is a mathematical symbol.
+    pub fn isMath(self: Unmanaged, cp: u21) bool {
+        return self.core_s2[self.core_s1[cp >> 8] + (cp & 0xff)] & 1 == 1;
+    }
+
+    /// True if `cp` is an alphabetic character.
+    pub fn isAlphabetic(self: Unmanaged, cp: u21) bool {
+        return self.core_s2[self.core_s1[cp >> 8] + (cp & 0xff)] & 2 == 2;
+    }
+
+    /// True if `cp` is a valid identifier start character.
+    pub fn isIdStart(self: Unmanaged, cp: u21) bool {
+        return self.core_s2[self.core_s1[cp >> 8] + (cp & 0xff)] & 4 == 4;
+    }
+
+    /// True if `cp` is a valid identifier continuation character.
+    pub fn isIdContinue(self: Unmanaged, cp: u21) bool {
+        return self.core_s2[self.core_s1[cp >> 8] + (cp & 0xff)] & 8 == 8;
+    }
+
+    /// True if `cp` is a valid extended identifier start character.
+    pub fn isXidStart(self: Unmanaged, cp: u21) bool {
+        return self.core_s2[self.core_s1[cp >> 8] + (cp & 0xff)] & 16 == 16;
+    }
+
+    /// True if `cp` is a valid extended identifier continuation character.
+    pub fn isXidContinue(self: Unmanaged, cp: u21) bool {
+        return self.core_s2[self.core_s1[cp >> 8] + (cp & 0xff)] & 32 == 32;
+    }
+
+    /// True if `cp` is a whitespace character.
+    pub fn isWhitespace(self: Unmanaged, cp: u21) bool {
+        return self.props_s2[self.props_s1[cp >> 8] + (cp & 0xff)] & 1 == 1;
+    }
+
+    /// True if `cp` is a hexadecimal digit.
+    pub fn isHexDigit(self: Unmanaged, cp: u21) bool {
+        return self.props_s2[self.props_s1[cp >> 8] + (cp & 0xff)] & 2 == 2;
+    }
+
+    /// True if `cp` is a diacritic mark.
+    pub fn isDiacritic(self: Unmanaged, cp: u21) bool {
+        return self.props_s2[self.props_s1[cp >> 8] + (cp & 0xff)] & 4 == 4;
+    }
+
+    /// True if `cp` is numeric.
+    pub fn isNumeric(self: Unmanaged, cp: u21) bool {
+        return self.num_s2[self.num_s1[cp >> 8] + (cp & 0xff)] & 1 == 1;
+    }
+
+    /// True if `cp` is a digit.
+    pub fn isDigit(self: Unmanaged, cp: u21) bool {
+        return self.num_s2[self.num_s1[cp >> 8] + (cp & 0xff)] & 2 == 2;
+    }
+
+    /// True if `cp` is decimal.
+    pub fn isDecimal(self: Unmanaged, cp: u21) bool {
+        return self.num_s2[self.num_s1[cp >> 8] + (cp & 0xff)] & 4 == 4;
+    }
+};
 
 const Self = @This();
 
+allocator: mem.Allocator,
+unmanaged: Unmanaged,
+
 pub fn init(allocator: mem.Allocator) !Self {
-    const decompressor = compress.flate.inflate.decompressor;
-    const endian = builtin.cpu.arch.endian();
-
-    // Process DerivedCoreProperties.txt
-    const core_bytes = @embedFile("core_props");
-    var core_fbs = std.io.fixedBufferStream(core_bytes);
-    var core_decomp = decompressor(.raw, core_fbs.reader());
-    var core_reader = core_decomp.reader();
-
-    var self = Self{ .allocator = allocator };
-
-    const core_stage_1_len: u16 = try core_reader.readInt(u16, endian);
-    self.core_s1 = try allocator.alloc(u16, core_stage_1_len);
-    errdefer allocator.free(self.core_s1);
-    for (0..core_stage_1_len) |i| self.core_s1[i] = try core_reader.readInt(u16, endian);
-
-    const core_stage_2_len: u16 = try core_reader.readInt(u16, endian);
-    self.core_s2 = try allocator.alloc(u8, core_stage_2_len);
-    errdefer allocator.free(self.core_s2);
-    _ = try core_reader.readAll(self.core_s2);
-
-    // Process PropList.txt
-    const props_bytes = @embedFile("props");
-    var props_fbs = std.io.fixedBufferStream(props_bytes);
-    var props_decomp = decompressor(.raw, props_fbs.reader());
-    var props_reader = props_decomp.reader();
-
-    const stage_1_len: u16 = try props_reader.readInt(u16, endian);
-    self.props_s1 = try allocator.alloc(u16, stage_1_len);
-    errdefer allocator.free(self.props_s1);
-    for (0..stage_1_len) |i| self.props_s1[i] = try props_reader.readInt(u16, endian);
-
-    const stage_2_len: u16 = try props_reader.readInt(u16, endian);
-    self.props_s2 = try allocator.alloc(u8, stage_2_len);
-    errdefer allocator.free(self.props_s2);
-    _ = try props_reader.readAll(self.props_s2);
-
-    // Process DerivedNumericType.txt
-    const num_bytes = @embedFile("numeric");
-    var num_fbs = std.io.fixedBufferStream(num_bytes);
-    var num_decomp = decompressor(.raw, num_fbs.reader());
-    var num_reader = num_decomp.reader();
-
-    const num_stage_1_len: u16 = try num_reader.readInt(u16, endian);
-    self.num_s1 = try allocator.alloc(u16, num_stage_1_len);
-    errdefer allocator.free(self.num_s1);
-    for (0..num_stage_1_len) |i| self.num_s1[i] = try num_reader.readInt(u16, endian);
-
-    const num_stage_2_len: u16 = try num_reader.readInt(u16, endian);
-    self.num_s2 = try allocator.alloc(u8, num_stage_2_len);
-    errdefer allocator.free(self.num_s2);
-    _ = try num_reader.readAll(self.num_s2);
-
-    return self;
+    return .{
+        .allocator = allocator,
+        .unmanaged = try Unmanaged.init(allocator),
+    };
 }
 
 pub fn deinit(self: *const Self) void {
-    self.allocator.free(self.core_s1);
-    self.allocator.free(self.core_s2);
-    self.allocator.free(self.props_s1);
-    self.allocator.free(self.props_s2);
-    self.allocator.free(self.num_s1);
-    self.allocator.free(self.num_s2);
+    self.unmanaged.deinit(self.allocator);
 }
 
 /// True if `cp` is a mathematical symbol.
 pub fn isMath(self: Self, cp: u21) bool {
-    return self.core_s2[self.core_s1[cp >> 8] + (cp & 0xff)] & 1 == 1;
+    return self.unmanaged.isMath(cp);
 }
 
 /// True if `cp` is an alphabetic character.
 pub fn isAlphabetic(self: Self, cp: u21) bool {
-    return self.core_s2[self.core_s1[cp >> 8] + (cp & 0xff)] & 2 == 2;
+    return self.unmanaged.isAlphabetic(cp);
 }
 
 /// True if `cp` is a valid identifier start character.
 pub fn isIdStart(self: Self, cp: u21) bool {
-    return self.core_s2[self.core_s1[cp >> 8] + (cp & 0xff)] & 4 == 4;
+    return self.unmanaged.isIdStart(cp);
 }
 
 /// True if `cp` is a valid identifier continuation character.
 pub fn isIdContinue(self: Self, cp: u21) bool {
-    return self.core_s2[self.core_s1[cp >> 8] + (cp & 0xff)] & 8 == 8;
+    return self.unmanaged.isIdContinue(cp);
 }
 
 /// True if `cp` is a valid extended identifier start character.
 pub fn isXidStart(self: Self, cp: u21) bool {
-    return self.core_s2[self.core_s1[cp >> 8] + (cp & 0xff)] & 16 == 16;
+    return self.unmanaged.isXidStart(cp);
 }
 
 /// True if `cp` is a valid extended identifier continuation character.
 pub fn isXidContinue(self: Self, cp: u21) bool {
-    return self.core_s2[self.core_s1[cp >> 8] + (cp & 0xff)] & 32 == 32;
+    return self.unmanaged.isXidContinue(cp);
 }
 
 /// True if `cp` is a whitespace character.
 pub fn isWhitespace(self: Self, cp: u21) bool {
-    return self.props_s2[self.props_s1[cp >> 8] + (cp & 0xff)] & 1 == 1;
+    return self.unmanaged.isWhitespace(cp);
 }
 
 /// True if `cp` is a hexadecimal digit.
 pub fn isHexDigit(self: Self, cp: u21) bool {
-    return self.props_s2[self.props_s1[cp >> 8] + (cp & 0xff)] & 2 == 2;
+    return self.unmanaged.isHexDigit(cp);
 }
 
 /// True if `cp` is a diacritic mark.
 pub fn isDiacritic(self: Self, cp: u21) bool {
-    return self.props_s2[self.props_s1[cp >> 8] + (cp & 0xff)] & 4 == 4;
+    return self.unmanaged.isDiacritic(cp);
 }
 
 /// True if `cp` is numeric.
 pub fn isNumeric(self: Self, cp: u21) bool {
-    return self.num_s2[self.num_s1[cp >> 8] + (cp & 0xff)] & 1 == 1;
+    return self.unmanaged.isNumeric(cp);
 }
 
 /// True if `cp` is a digit.
 pub fn isDigit(self: Self, cp: u21) bool {
-    return self.num_s2[self.num_s1[cp >> 8] + (cp & 0xff)] & 2 == 2;
+    return self.unmanaged.isDigit(cp);
 }
 
 /// True if `cp` is decimal.
 pub fn isDecimal(self: Self, cp: u21) bool {
-    return self.num_s2[self.num_s1[cp >> 8] + (cp & 0xff)] & 4 == 4;
+    return self.unmanaged.isDecimal(cp);
 }
 
 test "Props" {
